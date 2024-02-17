b43e0b72d8861d573037e1a5939cc2fe5693f5d4
Uproxide <153382689+Uproxide@users.noreply.github.com>
2024-02-16 20:46:15 -0500

    Fix two mods

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuGameLayer.hpp>
 #include <Geode/modify/MoreOptionsLayer.hpp>
+#include <Geode/loader/Loader.hpp>
 
 #include "IconDialogLabel.hpp"
 #include "IconDialogPopup.hpp"
@@ -12,31 +13,35 @@ using namespace geode::prelude;
 IconDialogLabel* iconLabel = nullptr;
 
 class $modify(MenuGameLayer) {
-	bool init() {
-		if (!MenuGameLayer::init())
-			return false;
-		
-		iconLabel = IconDialogLabel::create("I hate my job", "bigFont.fnt", this->m_playerObject);
-		iconLabel->setScale(0.4);
-		this->addChild(iconLabel);
-
-		iconLabel->iconRespawned();
-
-		return true;
-	} 
-
-	void resetPlayer() {
-		MenuGameLayer::resetPlayer();
-		if (iconLabel)
+	if (!Loader::get()->isModLoaded("iristraa.mainmenuwallpaper")) {
+		bool init() {
+			if (!MenuGameLayer::init())
+				return false;
+			
+			iconLabel = IconDialogLabel::create("I hate my job", "bigFont.fnt", this->m_playerObject);
+			iconLabel->setScale(0.4);
+			this->addChild(iconLabel);
+	
 			iconLabel->iconRespawned();
-	}
-
-	void destroyPlayer() {
-		if (iconLabel) {
-			IconDialogPopup::create(this->m_playerObject)->show();
-        }
-
-		MenuGameLayer::destroyPlayer();
+	
+			return true;
+		} 
+		
+		if (m_playerObject->isVisible()) {
+			void resetPlayer() {
+				MenuGameLayer::resetPlayer();
+				if (iconLabel)
+					iconLabel->iconRespawned();
+			}
+		
+			void destroyPlayer() {
+				if (iconLabel) {
+					IconDialogPopup::create(this->m_playerObject)->show();
+		        }
+		
+				MenuGameLayer::destroyPlayer();
+			}
+		}
 	}
 };
 
